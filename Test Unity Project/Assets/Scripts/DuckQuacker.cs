144fba1921b7bba095d69d0bf581e1deaf074e4f
Michaelwolf95 <michaelwolf95@gmail.com>
2018-09-28 16:49:51 -0700

    QUACK QUACK MOTHER FUCKER

---
+++ Test Unity Project/Assets/Scripts/DuckQuacker.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DuckQuacker : MonoBehaviour
+{
+    public AudioSource myAudioSource;
+
+    public float pitch = 1f;
+    public float sizeScalar = 1f;
+
+	void Update ()
+	{
+	    if (Input.GetKeyDown(KeyCode.Space))
+	    {
+	        myAudioSource.pitch = pitch;
+            transform.localScale = new Vector2(sizeScalar, sizeScalar);
+
+	        pitch = pitch + 0.1f;
+            sizeScalar = sizeScalar - 0.1f;
+
+            myAudioSource.Play();
+        }
+	}
+}
